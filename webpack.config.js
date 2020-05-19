e4adf4b8e33e63b5195a103d502d8987f8bc5e5d
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2020-05-19 21:33:12 +0200

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+	mode: 'development',
+	context: path.resolve(__dirname, 'example'),
+	entry: [
+		'./index.js',
+		'./index.css',
+		'./tools/dom_extension.js',
+	],
+	output: {
+		path: path.resolve(__dirname, 'example', 'dist'),
+		filename: '[name].bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'example', 'index.html'),
+			inject: 'head',
+			xhtml: true
+		}),
+		new CopyPlugin({
+			patterns: [
+				{from: '../test/resources/*.dcm', to: './test/[name].dcm'}
+			],
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.png$/,
+				use: [
+					'file-loader',
+				]
+			},
+			{
+				test: /workers/,
+				use: {
+					loader: 'worker-loader'
+				}
+			}
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'example', 'dist'),
+		port: 9000,
+		host: '0.0.0.0'
+	}
+};
